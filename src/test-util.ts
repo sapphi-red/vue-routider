880113cb0ef288844b5fd33688b1d3253a19269c
sapphi-red <green@sapphi.red>
2020-08-14 00:56:54 +0900

    Add more test

--- src/test-util.ts
+++ src/test-util.ts
@@ -6,3 +6,10 @@ export const isTypeEqual = <Left, Right>(
   void shouldBeEqual
   expect(true).toBe(true)
 }
+
+export const isSubType = <SuperType, SubType>(
+  shouldBeEqual: SubType extends SuperType ? true : false
+): void => {
+  void shouldBeEqual
+  expect(true).toBe(true)
+}
