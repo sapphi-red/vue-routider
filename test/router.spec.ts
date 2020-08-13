50e2ad56461d55bba0313e59f6b51ca8d5ffd20f
sapphi-red <green@sapphi.red>
2020-08-14 01:11:42 +0900

    Add tests for invalid path

--- test/router.spec.ts
+++ test/router.spec.ts
@@ -1,11 +1,101 @@
 import { createMemoryHistory } from 'vue-router'
-import { createRoutider, createPath, createPaths } from '#/index'
-import { isTypeEqual } from '#/test-util'
+import {
+  createRoutider,
+  createPath,
+  createPaths,
+  RoutiderOptionsRoutes
+} from '#/index'
+import { isSubType, isTypeEqual } from '#/test-util'
 import { defineComponent } from 'vue'
 
-describe('createRoutider', () => {
-  const com = defineComponent({})
+const com = defineComponent({})
 
+describe('routiderOptions', () => {
+  it('can detect valid path (1)', () => {
+    const options = {
+      Index: {
+        path: '/',
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(true)
+  })
+  it('can detect valid path (2)', () => {
+    const options = {
+      Index: {
+        path: createPath`/`,
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(true)
+  })
+  it('can detect valid path (3)', () => {
+    const options = {
+      Index: {
+        path: createPath`/item/${'id'}`,
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(true)
+  })
+  it('can detect valid path (4)', () => {
+    const options = {
+      Index: {
+        path: ['/', '/index'],
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(true)
+  })
+  it('can detect valid path (5)', () => {
+    const options = {
+      Index: {
+        path: createPaths(createPath`/items`, createPath`/i`),
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(true)
+  })
+  it('can detect valid path (6)', () => {
+    const options = {
+      Index: {
+        path: createPaths(createPath`/items/${'id'}`, createPath`/i/${'id'}`),
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(true)
+  })
+
+  it('can detect invalid path (1)', () => {
+    const options = {
+      Index: {
+        path: 0,
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(false)
+  })
+  it('can detect invalid path (2)', () => {
+    const options = {
+      Index: {
+        path: [createPath`/items`, createPath`/i`],
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(false)
+  })
+  it('can detect invalid path (3)', () => {
+    const options = {
+      Index: {
+        path: [createPath`/items/${'id'}`, createPath`/i/${'itemId'}`],
+        component: com
+      }
+    }
+    isSubType<RoutiderOptionsRoutes, typeof options>(false)
+  })
+})
+
+describe('createRoutider', () => {
   const { useRoute } = createRoutider({
     history: createMemoryHistory(),
     routes: {
