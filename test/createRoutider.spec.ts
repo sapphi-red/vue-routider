282574a14318427254aa8c50f789bd0a73d395fb
sapphi-red <green@sapphi.red>
2020-08-14 21:19:29 +0900

    Fix test

--- test/createRoutider.spec.ts
+++ test/createRoutider.spec.ts
@@ -1,27 +1,16 @@
 import { createMemoryHistory, Router } from 'vue-router'
 import { createRoutider, createPath } from '#/index'
 import { isSameType } from '#/test-util'
-import { defineComponent, createApp } from 'vue'
+import { defineComponent } from 'vue'
 import { mount } from '@vue/test-utils'
 
 type NeverRecord = Record<never, never>
 
-const Com = defineComponent({
-  template: '<div></div>'
-})
-
-const runInsideComponent = (routerInstall: Router, func: () => unknown) => {
+const getRouter = async (initPath = '/') => {
   const Com = defineComponent({
-    template: '<div></div>',
-    setup() {
-      func()
-      return {}
-    }
+    template: '<div></div>'
   })
-  mount(Com, { global: { plugins: [routerInstall] } })
-}
 
-describe('createRoutider', () => {
   const { router: routerInstall, useRoute, useRouter } = createRoutider({
     history: createMemoryHistory(),
     routes: {
@@ -38,31 +27,60 @@ describe('createRoutider', () => {
         component: Com
       },
       About: {
-        path: createPath`/`,
+        path: createPath`/about`,
         component: Com
       }
     }
   })
+  routerInstall.push(initPath)
+  await routerInstall.isReady()
+  return { routerInstall, useRoute, useRouter }
+}
 
-  const app = createApp(Com)
-  app.use(routerInstall)
+const runInsideComponent = (routerInstall: Router, func: () => unknown) => {
+  const Com = defineComponent({
+    template: '<div></div>',
+    setup() {
+      func()
+      return {}
+    }
+  })
+  mount(Com, { global: { plugins: [routerInstall] } })
+}
 
-  runInsideComponent(routerInstall, () => {
-    const router = useRouter()
-
-    it('has typed router.push (1)', () => {
+describe('createRoutider', () => {
+  it('has typed router.push (1)', async () => {
+    const { routerInstall, useRouter } = await getRouter()
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
       router.push({ name: 'About' })
     })
-    it('has typed router.push (2)', () => {
+  })
+  it('has typed router.push (2)', async () => {
+    const { routerInstall, useRouter } = await getRouter()
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
       router.push({ name: 'Index' })
     })
-    it('has typed router.push (3)', () => {
+  })
+  it('has typed router.push (3)', async () => {
+    const { routerInstall, useRouter } = await getRouter()
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
       router.push({ name: 'Item', params: { id: '1' } })
     })
-    it('has typed router.push (4)', () => {
+  })
+  it('has typed router.push (4)', async () => {
+    const { routerInstall, useRouter } = await getRouter()
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
       router.push({ name: 'UserItem', params: { id: '1', userId: '1' } })
     })
-    it('can accept genetic route with router.push', () => {
+  })
+  it('can accept genetic route with router.push', async () => {
+    const { routerInstall, useRouter } = await getRouter()
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
       const rs = [
         { name: 'About' },
         { name: 'Index' },
@@ -75,82 +93,71 @@ describe('createRoutider', () => {
     })
   })
 
-  it('has typed route (1)', () => {
+  it('has typed route (1)', async () => {
+    const { routerInstall, useRoute } = await getRouter('/items/1')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'Item', params: { id: '1' } })
-
       const route = useRoute('Item')
       isSameType<{ id: string }, typeof route.params>(true)
     })
   })
-
-  it('has typed route (2)', () => {
+  it('has typed route (2)', async () => {
+    const { routerInstall, useRoute } = await getRouter('/users/1/1')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'UserItem', params: { id: '1', userId: '1' } })
-
       const route = useRoute('UserItem')
       isSameType<{ userId: string; id: string }, typeof route.params>(true)
     })
   })
-
-  it('has typed route (3)', () => {
+  it('has typed route (3)', async () => {
+    const { routerInstall, useRoute } = await getRouter('/')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'Index' })
-
       const route = useRoute('Index')
       isSameType<NeverRecord, typeof route.params>(true)
     })
   })
-
-  it('has typed route (4)', () => {
+  it('has typed route (4)', async () => {
+    const { routerInstall, useRoute } = await getRouter('/about')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'About' })
-
       const route = useRoute('About')
       isSameType<NeverRecord, typeof route.params>(true)
     })
   })
 
-  it('has intersection and union typed route', () => {
+  it('has intersection and union typed route', async () => {
+    const { routerInstall, useRoute } = await getRouter('/items/1')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'Item', params: { id: '1' } })
-
       const route = useRoute(['Item', 'UserItem'])
       isSameType<{ userId?: string; id: string }, typeof route.params>(true)
     })
   })
-  it('has union typed route', () => {
+  it('has union typed route', async () => {
+    const { routerInstall, useRoute } = await getRouter('/about')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'About' })
-
       const route = useRoute(['Item', 'UserItem', 'About'])
       isSameType<{ userId?: string; id?: string }, typeof route.params>(true)
     })
   })
-  it('has intersection typed route', () => {
+  it('has intersection typed route', async () => {
+    const { routerInstall, useRoute } = await getRouter('/')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'Index' })
-
       const route = useRoute(['Index', 'About'])
       isSameType<NeverRecord, typeof route.params>(true)
     })
   })
 
-  it('has untyped route', () => {
+  it('has untyped route', async () => {
+    const { routerInstall, useRoute } = await getRouter('/')
     runInsideComponent(routerInstall, () => {
-      const router = useRouter()
-      router.push({ name: 'Index' })
-
       const route = useRoute(null)
-
       isSameType<Record<string, string | undefined>, typeof route.params>(true)
+    })
+  })
+
+  it('warns if useRoute is called in incorrect path', async () => {
+    const { routerInstall, useRoute } = await getRouter('/')
+    runInsideComponent(routerInstall, () => {
+      console.warn = jest.fn()
+      useRoute('Item')
+      expect(console.warn).toBeCalled()
     })
   })
 })
