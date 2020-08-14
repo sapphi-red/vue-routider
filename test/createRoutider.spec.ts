aacfec32a3fa8d1795c0864d08265ceeb9e409b9
sapphi-red <green@sapphi.red>
2020-08-14 20:27:03 +0900

    Use vue-test-utils

--- test/createRoutider.spec.ts
+++ test/createRoutider.spec.ts
@@ -1,103 +1,156 @@
-import { createMemoryHistory } from 'vue-router'
+import { createMemoryHistory, Router } from 'vue-router'
 import { createRoutider, createPath } from '#/index'
 import { isSameType } from '#/test-util'
-import { defineComponent } from 'vue'
+import { defineComponent, createApp } from 'vue'
+import { mount } from '@vue/test-utils'
 
 type NeverRecord = Record<never, never>
 
-const com = defineComponent({})
+const Com = defineComponent({
+  template: '<div></div>'
+})
+
+const runInsideComponent = (routerInstall: Router, func: () => unknown) => {
+  const Com = defineComponent({
+    template: '<div></div>',
+    setup() {
+      func()
+      return {}
+    }
+  })
+  mount(Com, { global: { plugins: [routerInstall] } })
+}
 
 describe('createRoutider', () => {
-  const { useRoute, useRouter } = createRoutider({
+  const { router: routerInstall, useRoute, useRouter } = createRoutider({
     history: createMemoryHistory(),
     routes: {
       Index: {
         path: '/',
-        component: com
+        component: Com
       },
       Item: {
         path: createPath`/items/${'id'}`,
-        component: com
+        component: Com
       },
       UserItem: {
         path: createPath`/users/${'userId'}/${'id'}`,
-        component: com
+        component: Com
       },
       About: {
         path: createPath`/`,
-        component: com
+        component: Com
       }
     }
   })
-  const router = useRouter()
+
+  const app = createApp(Com)
+  app.use(routerInstall)
+
+  runInsideComponent(routerInstall, () => {
+    const router = useRouter()
+
+    it('has typed router.push (1)', () => {
+      router.push({ name: 'About' })
+    })
+    it('has typed router.push (2)', () => {
+      router.push({ name: 'Index' })
+    })
+    it('has typed router.push (3)', () => {
+      router.push({ name: 'Item', params: { id: '1' } })
+    })
+    it('has typed router.push (4)', () => {
+      router.push({ name: 'UserItem', params: { id: '1', userId: '1' } })
+    })
+    it('can accept genetic route with router.push', () => {
+      const rs = [
+        { name: 'About' },
+        { name: 'Index' },
+        { name: 'About', params: { id: '1' } }
+      ] as const
+
+      rs.forEach(r => {
+        router.push(r)
+      })
+    })
+  })
 
   it('has typed route (1)', () => {
-    const route = useRoute('Item')
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'Item', params: { id: '1' } })
 
-    isSameType<{ id: string }, typeof route.params>(true)
+      const route = useRoute('Item')
+      isSameType<{ id: string }, typeof route.params>(true)
+    })
   })
 
   it('has typed route (2)', () => {
-    const route = useRoute('UserItem')
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'UserItem', params: { id: '1', userId: '1' } })
 
-    isSameType<{ userId: string; id: string }, typeof route.params>(true)
+      const route = useRoute('UserItem')
+      isSameType<{ userId: string; id: string }, typeof route.params>(true)
+    })
   })
 
   it('has typed route (3)', () => {
-    const route = useRoute('Index')
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'Index' })
 
-    isSameType<NeverRecord, typeof route.params>(true)
+      const route = useRoute('Index')
+      isSameType<NeverRecord, typeof route.params>(true)
+    })
   })
 
   it('has typed route (4)', () => {
-    const route = useRoute('About')
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'About' })
 
-    isSameType<NeverRecord, typeof route.params>(true)
+      const route = useRoute('About')
+      isSameType<NeverRecord, typeof route.params>(true)
+    })
   })
 
   it('has intersection and union typed route', () => {
-    const route = useRoute(['Item', 'UserItem'])
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'Item', params: { id: '1' } })
 
-    isSameType<{ userId?: string; id: string }, typeof route.params>(true)
+      const route = useRoute(['Item', 'UserItem'])
+      isSameType<{ userId?: string; id: string }, typeof route.params>(true)
+    })
   })
   it('has union typed route', () => {
-    const route = useRoute(['Item', 'UserItem', 'About'])
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'About' })
 
-    isSameType<{ userId?: string; id?: string }, typeof route.params>(true)
+      const route = useRoute(['Item', 'UserItem', 'About'])
+      isSameType<{ userId?: string; id?: string }, typeof route.params>(true)
+    })
   })
   it('has intersection typed route', () => {
-    const route = useRoute(['Index', 'About'])
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'Index' })
 
-    isSameType<NeverRecord, typeof route.params>(true)
+      const route = useRoute(['Index', 'About'])
+      isSameType<NeverRecord, typeof route.params>(true)
+    })
   })
 
   it('has untyped route', () => {
-    const route = useRoute(null)
+    runInsideComponent(routerInstall, () => {
+      const router = useRouter()
+      router.push({ name: 'Index' })
 
-    isSameType<Record<string, string | undefined>, typeof route.params>(true)
-  })
+      const route = useRoute(null)
 
-  it('has typed router.push (1)', () => {
-    router.push({ name: 'About' })
-  })
-  it('has typed router.push (2)', () => {
-    router.push({ name: 'Index' })
-  })
-  it('has typed router.push (3)', () => {
-    router.push({ name: 'Item', params: { id: '1' } })
-  })
-  it('has typed router.push (4)', () => {
-    router.push({ name: 'UserItem', params: { id: '1', userId: '1' } })
-  })
-  it('can accept genetic route with router.push', () => {
-    const rs = [
-      { name: 'About' },
-      { name: 'Index' },
-      { name: 'About', params: { id: '1' } }
-    ] as const
-
-    rs.forEach(r => {
-      router.push(r)
+      isSameType<Record<string, string | undefined>, typeof route.params>(true)
     })
   })
 })
