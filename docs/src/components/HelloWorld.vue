56399172b894e2594365490d7801ec2f90b7e369
sapphi-red <green@sapphi.red>
2020-08-15 16:59:40 +0900

    [docs] Add lint

--- docs/src/components/HelloWorld.vue
+++ docs/src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
 <script>
@@ -10,7 +12,10 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      default: 'default'
+    }
   },
   data() {
     return {
